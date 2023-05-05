07f02f718021e235eaca6057a9c89190ca589207
daniel <jabodav.daniel@gmail.com>
2023-05-05 15:44:21 +0300

    Optimized id generator

--- src/main/java/com/javatechie/spring/ajax/api/model/Book.java
+++ src/main/java/com/javatechie/spring/ajax/api/model/Book.java
@@ -1,14 +1,10 @@
 package com.javatechie.spring.ajax.api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.GenericGenerator;
 
 @Data
 @AllArgsConstructor
@@ -19,8 +15,11 @@ public class Book
 {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
+    @SequenceGenerator(
+            name = "book_seq",
+            sequenceName = "book_seq"
+    )
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "book_seq")
     private int id;
     private String bookName;
     private String author;
